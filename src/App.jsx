c19f277b5e094cd25be3174e2b41b36feb15157c
Talha <taldemirtas@gmail.com>
2023-02-07 23:32:36 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Vite + React</h1>
+      <p className="read">
+        Vite and React
+      </p>
+    </div>
+  )
+}
+
+export default App
