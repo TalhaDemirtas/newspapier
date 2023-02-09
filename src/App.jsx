64d9539b0a3b6be8ba1917a296ae62c31716cf1b
Talha <taldemirtas@gmail.com>
2023-02-09 23:11:32 +0300

    refactor: rename FetchData as Category

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import FetchData from "./components/Category";
+import Header from './components/Header';
+import Home from './pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Category from './components/Category';
 
 function App() {
-
   return (
     <>
-<BrowserRouter>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route exact path="/:cat_name" element={<FetchData/>} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/:cat_name" element={<Category />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
