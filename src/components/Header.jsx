8f8885d0d9839bcd1a5009a806d44a67eaf72ed0
Talha <taldemirtas@gmail.com>
2023-02-09 23:15:22 +0300

    refactor: navlink to link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,36 @@
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Navi from "./navi"
+import Navi from './navi';
 
 const Header = () => {
   return (
     <div>
-    <Navi />
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-        <Navbar.Brand className='font-weight-bold' style={{fontSize:"30px", color:"aquamarine"}}>Topics</Navbar.Brand>
-          <Nav className="me-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/general">General</Nav.Link>
-            <Nav.Link href="/business">Bussiness</Nav.Link>
-            <Nav.Link href="/health">Health</Nav.Link>
-            <Nav.Link href="/science">Science</Nav.Link>
-            <Nav.Link href="/entertainment">Entertainment</Nav.Link>
-            <Nav.Link href="/sports">Sports</Nav.Link>
-            <Nav.Link href="/technology">Tech</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <Navi />
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Navbar.Brand
+              className="font-weight-bold"
+              style={{ fontSize: '30px', color: 'aquamarine' }}
+            >
+              Topics
+            </Navbar.Brand>
+            <Nav className="me-auto">
+              <Link to="/" className='nav-link'>Home</Link>
+              <Link to="/general" className='nav-link'>General</Link>
+              <Link to="/business" className='nav-link'>Bussiness</Link>
+              <Link to="/health" className='nav-link'>Health</Link>
+              <Link to="/science" className='nav-link'>Science</Link>
+              <Link to="/entertainment" className='nav-link'>Entertainment</Link>
+              <Link to="/sports" className='nav-link'>Sports</Link>
+              <Link to="/technology" className='nav-link'>Tech</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
